933220b9424f46d242018567d5524d5affc35017
Diego <iwujigoodluck5@gmail.com>
2025-06-14 08:55:53 +0000

    Update cocbase.js

--- src/utils/cocbase.js
+++ src/utils/cocbase.js
@@ -2,7 +2,6 @@ import { Cocobase } from "cocobase";
 
 const cb = new Cocobase({
 	apiKey: import.meta.env.VITE_COCOBASE_API_KEY, // Your API key
-	// projectId: import.meta.env.VITE_COCOBASE_PROJECT_ID, // Your project ID
 	environment: "development", // or 'development'
 });
 
